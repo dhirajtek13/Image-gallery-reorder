de8805861dcdd7966a20228930c35de978b1077f
dhirajtek13 <dhirajtek13@gmail.com>
2024-01-05 21:13:47 +0530

    format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-import imageData from './imagesData.json';
-import Header from './components/Header';
-import Breadcrumb from './components/Breadcrumb';
-import TagButton from './components/TagButton';
-import ImageGrid from './components/ImageGrid';
-import ResetButton from './components/ResetButton';
-
+import imageData from "./imagesData.json";
+import Header from "./components/Header";
+import Breadcrumb from "./components/Breadcrumb";
+import TagButton from "./components/TagButton";
+import ImageGrid from "./components/ImageGrid";
+import ResetButton from "./components/ResetButton";
 
 const App = () => {
   const [images, setImages] = useState([]);
   const [tags, setTags] = useState([]);
-  const [activeTag, setActiveTag] = useState('all');
-  
+  const [activeTag, setActiveTag] = useState("all");
 
   useEffect(() => {
-    const storedImages = JSON.parse(localStorage.getItem('images')) || imageData;
+    const storedImages =
+      JSON.parse(localStorage.getItem("images")) || imageData;
     const allTags = storedImages.reduce((all, image) => {
       if (image.tags) {
         return [...all, ...image.tags];
@@ -32,11 +31,11 @@ const App = () => {
 
   const filterImages = (selectedTag) => {
     setActiveTag(selectedTag);
-    if (selectedTag === 'all') {
+    if (selectedTag === "all") {
       setImages(imageData);
     } else {
-      const filteredImages = imageData.filter((image) =>
-        image.tags && image.tags.includes(selectedTag)
+      const filteredImages = imageData.filter(
+        (image) => image.tags && image.tags.includes(selectedTag)
       );
       setImages(filteredImages);
     }
@@ -48,34 +47,40 @@ const App = () => {
     reorderedImages.splice(toIndex, 0, movedImage);
 
     setImages(reorderedImages);
-    localStorage.setItem('images', JSON.stringify(reorderedImages));
+    localStorage.setItem("images", JSON.stringify(reorderedImages));
   };
 
   const resetOrder = () => {
     setImages(imageData);
-    localStorage.setItem('images', JSON.stringify(imageData));
+    localStorage.setItem("images", JSON.stringify(imageData));
   };
-
 
   return (
     <DndProvider backend={HTML5Backend}>
-    <div className="App">
-    <Header />
-    <Breadcrumb />
-      <h1>Photo Gallery</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
-    
-      <div className="tag-buttons">
-          <ResetButton onClick={resetOrder} />
-          <TagButton tag="all" isActive={activeTag === 'all' && 'active'}  onClick={() => filterImages('all')} />
-          {tags.map((tag) => (
-            <TagButton tag={tag} isActive={activeTag === tag && 'active'}  onClick={() => filterImages(tag)} />
-          ))}
+      <div className="App">
+        <Header />
+        <Breadcrumb />
+        <h1>Photo Gallery</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
 
+        <div className="tag-buttons">
+          <ResetButton onClick={resetOrder} />
+          <TagButton
+            tag="all"
+            isActive={activeTag === "all" && "active"}
+            onClick={() => filterImages("all")}
+          />
+          {tags.map((tag) => (
+            <TagButton
+              tag={tag}
+              isActive={activeTag === tag && "active"}
+              onClick={() => filterImages(tag)}
+            />
+          ))}
         </div>
 
-      <ImageGrid images={images} moveImage={moveImage} />
-    </div>
+        <ImageGrid images={images} moveImage={moveImage} />
+      </div>
     </DndProvider>
   );
 };
